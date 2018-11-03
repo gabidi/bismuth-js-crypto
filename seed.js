d0840536df142acba01fbcc16b139356e9728ad8
gus <gabidi@gmail.com>
2018-11-03 14:46:17 -0400

    Tests pass, @Todo clean up utils

--- seed.js
+++ seed.js
@@ -1,29 +1,21 @@
-const _Crypto = require('./lib/cryptojs')
-const _SecureRandom = require('./lib/secureRandom')
-
-module.exports = ({
-  crypto = _Crypto(),
-  secureRandom = _SecureRandom({ crypto })
-} = {}) => {
+const _secureRandom = require('./lib/secureRandom')
+const { util } = require('./lib/cryptoUtils')()
+module.exports = ({ secureRandom = _secureRandom() } = {}) => {
   // number of mouse movements to wait for
-  const num = crypto.util.randomBytes(12)[11]
+  const num = util.randomBytes(12)[11]
   // return 200 + Math.floor(num);
   const seedLimit = 50 + Math.floor(num)
 
   let seedCount = 0 // counter
   let lastInputTime = new Date().getTime()
   let isStillSeeding = true
-
-  // const seedPoints = [];
-  // const seederDependentWallets = ["singlewallet", "paperwallet", "bulkwallet", "vanitywallet", "splitwallet"];
-
   // seed function exists to wait for mouse movement to add more entropy before generating an address
   const seedFromMouseMovementEvent = ({ clientX, clientY }) => {
     const timeStamp = new Date().getTime()
     // seeding is over now we generate and display the address
     if (seedCount === seedLimit) {
       seedCount++
-      seedingOver()
+      _seedingOver()
     }
     // seed mouse position X and Y when mouse movements are greater than 40ms apart.
     else if (seedCount < seedLimit && timeStamp - lastInputTime > 40) {
@@ -40,7 +32,7 @@ module.exports = ({
     // seeding is over now we generate and display the address
     if (seedCount === seedLimit) {
       seedCount++
-      seedingOver()
+      _seedingOver()
     }
     // seed key press character
     else if (seedCount < seedLimit && evt.which) {
@@ -58,70 +50,27 @@ module.exports = ({
 
   let poolHex, percentSeeded
 
-  /**
-   * FIXME Move to VIEW
-   */
   const updateSeedStats = function () {
     if (secureRandom.poolCopyOnInit != null) {
-      poolHex = crypto.util.bytesToHex(secureRandom.poolCopyOnInit)
-      // document.getElementById("seedpool").innerHTML = poolHex;
-      // document.getElementById("seedpooldisplay").innerHTML = poolHex;
+      poolHex = util.bytesToHex(secureRandom.poolCopyOnInit)
     } else {
-      poolHex = crypto.util.bytesToHex(secureRandom.pool)
-      // document.getElementById("seedpool").innerHTML = poolHex;
-      // document.getElementById("seedpooldisplay").innerHTML = poolHex;
+      poolHex = util.bytesToHex(secureRandom.pool)
     }
     percentSeeded = Math.round((seedCount / seedLimit) * 100) + '%'
-    // document.getElementById("mousemovelimit").innerHTML = percentSeeded;
-    /* for (var wIndex in ninja.seeder.seederDependentWallets) {
-            document.getElementById(ninja.seeder.seederDependentWallets[wIndex]).innerHTML = percentSeeded;
-        } */
+    console.log(percentSeeded, isStillSeeding)
   }
 
-  /*
-    const showPoint = function (x, y) {
-        const div = document.createElement("div");
-        const num = Math.floor((Math.random() * 6));
-        div.setAttribute("class", "seedpoint" + num);
-        div.style.top = y + "px";
-        div.style.left = x + "px";
-        document.body.appendChild(div);
-        ninja.seeder.seedPoints.push(div);
-    };
-*/
-
-  /*    const removePoints = function () {
-        for (let i = 0; i < ninja.seeder.seedPoints.length; i++) {
-            document.body.removeChild(ninja.seeder.seedPoints[i]);
-        }
-        ninja.seeder.seedPoints = [];
-    }; */
-
-  const seedingOver = () => {
+  const _seedingOver = () => {
     isStillSeeding = false
-
-    // run sync unit tests
-    // ninja.status.unitTests();
-    // open selected tab
-    /*  const walletType = null; //ninja.tab.whichIsOpen();
-        if (walletType == null) {
-            ninja.tab.select("singlewallet");
-        } else {
-            ninja.tab.select(walletType)
-        }
-        document.getElementById("generate").style.display = "none";
-        document.getElementById("generated").style.display = "block";
-        // update labels for dependent wallets
-        const culture = (ninja.getQueryString()["culture"] == null ? "en" : ninja.getQueryString()["culture"]);
-        //ninja.translator.translate(culture);
-        removePoints(); */
   }
-
+  const seedingDone = () => !isStillSeeding
+  const getSeedingPercent = () => percentSeeded
+  const getPoolHex = () => poolHex
   return {
     seedFromMouseMovementEvent,
     seedFromKeyPress,
-    isStillSeeding,
-    poolHex,
-    percentSeeded
+    seedingDone,
+    getPoolHex,
+    getSeedingPercent
   }
 }
---
+++ specs/seed2.spec.js
@@ -0,0 +1,95 @@
+const crypto = require('crypto')
+const _secureRandom = require('../lib/secureRandom')
+const _seed = require('../seed')
+const bip39 = require('bip39')
+const sha256 = require('sha256')
+const _arc4 = require('../lib/arc4')
+const forge = require('node-forge')
+const { expect } = require('chai')
+const { spy } = require('sinon')
+it('BIP 39 should generates the same seed/12 word seed result as python', async () => {
+  [
+    {
+      index: 0,
+      entropy: '3b13f9cb9ddea905883fa8d3ff7b1247',
+      twelveWords:
+        'deposit panther indicate desert tunnel lizard can vital stadium wink setup moment'
+    },
+    {
+      index: 1,
+      entropy: 'e1f9700dee95018fa20d47164fb5ed41',
+      twelveWords:
+        'tiger slide address tag exotic sick market pottery bind laundry kitten lonely'
+    },
+    {
+      index: 2,
+      entropy: 'ca1a79386919b745cefc0e50dec6afa3',
+      twelveWords:
+        'skate stable evoke split opinion pepper desk limb express wage program elder'
+    }
+  ].forEach(({ entropy, twelveWords }) => {
+    expect(bip39.entropyToMnemonic(entropy)).to.be.equal(twelveWords)
+    expect(bip39.mnemonicToEntropy(twelveWords).toString('hex')).to.be.equal(
+      entropy
+    )
+  })
+})
+it('Should be able to generate entropy using user data and generate mixed BIP39 mnemonic', async () => {
+  const secureRandom = _secureRandom({
+    entropyStr: 'test',
+    crypto
+  })
+  const seeder = _seed({
+    secureRandom
+  })
+  secureRandom.seedTime()
+  while (!seeder.seedingDone()) {
+    // Fake Random mouse
+    seeder.seedFromMouseMovementEvent({
+      XClient: Math.floor(Math.random() * 800),
+      YClient: Math.floor(Math.random() * 600)
+    })
+    await new Promise(resolve => setTimeout(resolve, 50))
+  }
+  expect(secureRandom.pool.length).to.be.at.least(256)
+  const seed = sha256(secureRandom.pool).slice(0, 32)
+  expect(seed.length).to.be.equal(32)
+  const twelveWords = bip39.entropyToMnemonic(seed)
+  expect(twelveWords.split(' ').length).to.be.equal(12)
+}).timeout(30000)
+it('Should be able to take an mnemonic seed and make an RSA key and BIS address out of it', async () => {
+  const mnemonic = {
+    index: 0,
+    entropy: '3b13f9cb9ddea905883fa8d3ff7b1247',
+    twelvewords:
+      'deposit panther indicate desert tunnel lizard can vital stadium wink setup moment'
+  }
+  //  mix with user passphrase /salet
+  const arc4Seed = bip39.mnemonicToSeed(mnemonic + ' mypassphrase')
+  //  feed RC4 as generator function to RSA
+  const rng = _arc4()
+  rng.init(arc4Seed)
+  // dispose first 300 bytes
+  for (let i = 1; i <= 300; i++) {
+    expect(rng.next()).to.be.an('Number')
+  }
+  // Set up spy on arc4 next to make sure it gets called by Rsa via nextBytes interface
+  const arc4Spy = spy(rng, 'nextBytesAsString')
+  // Pass it to RSA
+  const { privateKey, publicKey } = await new Promise((res, rej) => {
+    forge.pki.rsa.generateKeyPair(
+      {
+        prng: {
+          getBytesSync: rng.nextBytesAsString
+        },
+        bits: 4096,
+        workers: 4
+      },
+      (err, key) => (err ? rej(err) : res(key))
+    )
+  })
+  expect(arc4Spy.called).to.be.true
+  expect(forge.pki.privateKeyToPem(privateKey)).to.be.contain('BEGIN PRIVATE KEY')
+  expect(forge.pki.publicKeyToPem(publicKey)).to.be.contain('BEGIN PUBLIC KEY')
+  const bisAddress = sha224(forge.pki.publicKeyToPem(publicKey))
+}).timeout(360000)
