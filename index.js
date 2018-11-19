906883d39e4a8f3b1bfdc1f3f50ea05581b38617
gus <gabidi@gmail.com>
2018-11-19 10:43:08 -0500

    Entropy export

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const seed = require('./seed')
 const generate = require('./generate')
+const entropy = require('./entropy')
 const browserEntropy = require('./browser_crypto')
 module.exports = {
   seed,
   generate,
+  entropy,
   browserEntropy
 }
